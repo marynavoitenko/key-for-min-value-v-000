2686807c320fa5ddb31b8f683ef78cf36bf4d16a
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-10-28 22:12:58 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,14 @@
 
 def key_for_min_value(name_hash)
   return nil if name_hash.empty?
-  min_key = ""
-  min_value = 0
-  name_hash.each do |key, value|
-    if value < min_value
-      min_value = value
-    end
+  values = []
+  values = name_hash.collect { |key, value| value}
+  min_value = values[0]
+  i = 0
+  while i < values.length
+    min_value = values[i] if values[i] < min_value
+    i += 1
   end
-  min_value
+
+  name_hash.key(min_value)
 end
